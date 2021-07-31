66d91210bae4592f820222795e9fef98b1a37c4d
Marek <marek.oleksik@gmail.com>
2021-07-31 12:07:11 +0200

    refactor App class - extracted methods

--- src/main/java/pl/marekjava/App.java
+++ src/main/java/pl/marekjava/App.java
@@ -6,11 +6,40 @@ import java.util.Scanner;
 
 // This is a main class of Wheel of Fortune game
 public class App {
+    private static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+
         System.out.println("Witaj w Kole Fortuny");
 
         // Ask how many players will play
+        Integer numberOfPlayers = getNumberOfPlayers(scan);
+        if (numberOfPlayers == null) return;
+
+        // Add name of player to list of players
+        List<Player> players = getNamesOfPlayers(numberOfPlayers);
+        if (players == null) return;
+
+        // close Scanner
+        scan.close();
+    }
+
+    private static List<Player> getNamesOfPlayers(Integer numberOfPlayers) {
+        List<Player> players = new ArrayList<>();
+        for (int i = 0; i < numberOfPlayers; i++) {
+            System.out.print("Podaj imię gracza: ");
+            String name = scan.nextLine();
+            try {
+                players.add(new Player(name));
+            } catch (IllegalArgumentException ex) {
+                System.out.println("Imię nie może być puste");
+                return null;
+            }
+        }
+        return players;
+    }
+
+    private static Integer getNumberOfPlayers(Scanner scan) {
         int numberOfPlayers = 0;
         System.out.print("Podaj liczbę graczy: ");
         try {
@@ -21,23 +50,8 @@ public class App {
 
         if (numberOfPlayers < 2 || numberOfPlayers > 4) {
             System.out.println("Ilość graczy powinna zmieścić się pomiędzy 2 a 4.");
-            return;
+            return null;
         }
-
-        // Add name of player to list of players
-        List<Player> players = new ArrayList<>();
-        for (int i = 0; i < numberOfPlayers; i++) {
-            System.out.print("Podaj imię gracza: ");
-            String name = scan.nextLine();
-            try {
-                players.add(new Player(name));
-            } catch (IllegalArgumentException ex) {
-                System.out.println("Imię nie może być puste");
-                return;
-            }
-        }
-
-        // close Scanner
-        scan.close();
+        return numberOfPlayers;
     }
 }
